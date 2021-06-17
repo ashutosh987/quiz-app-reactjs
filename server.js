11da2508abcd53530110c3abb5457d370b2b5cd9
ashutosh987 <guptaashutosh427@gmail.com>
2021-06-17 18:56:15 +0530

    updated

--- server.js
+++ server.js
@@ -10,9 +10,18 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+app.get("/", (req, res) => res.send("api running"));
+
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 var server = app.listen(process.env.PORT || 5000, listening);
-app.get("/", (req, res) => res.send("api running"));
 
 function listening() {
   console.log("server is running");
